6a6b352fe84aa8f98b479c655be1038a4cffafe9
Asko Nõmm <asko@repl.ee>
2023-06-20 18:52:38 +0300

    bump

--- Invobi/Views/ContentView.swift
+++ Invobi/Views/ContentView.swift
@@ -15,14 +15,25 @@ struct SplashView: View {
     var body: some View {
         HStack(alignment: .center) {
             VStack(alignment: .center) {
-                Image("icon")
-                    .resizable()
-                    .frame(width: 110, height: 110)
+                if colorScheme == .dark {
+                    Image("icon")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .opacity(0.5)
+                } else {
+                    Image("icon")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                
                 Spacer().frame(height: 10)
+                
                 Text("You have no invoices yet. ")
                     .font(.title3)
                     .foregroundColor(colorScheme == .dark ? Color(hex: "#ddd") : Color(hex: "#777"))
+                
                 Spacer().frame(height: 40)
+                
                 Button(action: addInvoice) {
                     Label("Create Invoice", systemImage: "plus")
                 }
