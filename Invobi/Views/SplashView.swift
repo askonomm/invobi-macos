e843eea619a4440468f0b2ec0d1b9b331bcdb3d8
Asko Nõmm <asko@repl.ee>
2023-07-04 16:51:55 +0300

    bump

--- Invobi/Views/SplashView.swift
+++ Invobi/Views/SplashView.swift
@@ -17,10 +17,10 @@ struct SplashView: View {
             VStack(alignment: .center) {
                 Image("Icon")
                     .resizable()
-                    .cornerRadius(20)
-                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.1), radius: 20, y: 2)
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color(red: 0, green: 0, blue: 0, opacity: 0.2), lineWidth: 1))
-                    .frame(width: 65, height: 65)
+                    .cornerRadius(35)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 25, y: 2)
+                    .overlay(RoundedRectangle(cornerRadius: 35).stroke(.white, lineWidth: 2))
+                    .frame(width: 60, height: 60)
                 
                 Spacer().frame(height: 15)
                 
@@ -28,7 +28,7 @@ struct SplashView: View {
                     Circle()
                         .fill(.white)
                         .frame(width: 8, height: 8)
-                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.1), radius: 2, y: 2)
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.05), radius: 1, y: 1)
                         .offset(x: 0)
                     
                     Spacer().frame(height: 5)
@@ -36,7 +36,7 @@ struct SplashView: View {
                     Circle()
                         .fill(.white)
                         .frame(width: 11, height: 11)
-                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.1), radius: 2, y: 2)
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.05), radius: 1, y: 1)
                         .offset(x: -14)
                     
                     Spacer().frame(height: 5)
@@ -44,37 +44,35 @@ struct SplashView: View {
                     Circle()
                         .fill(.white)
                         .frame(width: 15, height: 15)
-                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.1), radius: 2, y: 2)
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.05), radius: 1, y: 1)
                         .offset(x: -4)
                 }
                 
                 Spacer().frame(height: 20)
                 
-                Text("Beep-boop, could not find invoices.")
+                Text("Oh-no! There's no invoices!")
                     .font(.body)
                     .foregroundColor(.black)
                     .padding(.horizontal, 8)
                     .padding(.vertical, 5)
                     .background(.white)
                     .cornerRadius(10)
-                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.1), radius: 2, y: 2)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.05), radius: 1, y: 1)
                 
                 Spacer().frame(height: 50)
                 
                 Button(action: addInvoice) {
                     Label("Create Invoice", systemImage: "plus")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .background(getGradient("button-bg"))
+                        .cornerRadius(25)
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 20, y: 2)
                 }
-                .font(.title3)
-                .fontWeight(.semibold)
-                .foregroundColor(.white)
                 .buttonStyle(.plain)
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
-                .background(LinearGradient(colors: [Color(hex: "#00BF13"), Color(hex: "#00B512")], startPoint: .top, endPoint: .bottom))
-                .cornerRadius(25)
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 20, y: 2)
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color(red: 0, green: 0, blue: 0, opacity: 0.4), lineWidth: 1))
-                
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -86,8 +84,12 @@ struct SplashView: View {
         switch k {
         case "bg":
             return colorScheme == .dark ?
-                LinearGradient(colors: [Color(hex: "#003605"), Color(hex: "#003605")], startPoint: .top, endPoint: .bottom) :
-                LinearGradient(colors: [Color(hex: "#AFFFA1"), Color(hex: "#85FF91")], startPoint: .top, endPoint: .bottom)
+                LinearGradient(colors: [Color(hex: "#0086E8"), Color(hex: "#7B00C6")], startPoint: .top, endPoint: .bottom) :
+                LinearGradient(colors: [Color(hex: "#EBF7FF"), Color(hex: "#ECCEFF")], startPoint: .top, endPoint: .bottom)
+        case "button-bg":
+            return colorScheme == .dark ?
+                LinearGradient(colors: [Color(hex: "#2E005F"), Color(hex: "#110041")], startPoint: .top, endPoint: .bottom) :
+                LinearGradient(colors: [Color(hex: "#B775FF"), Color(hex: "#8E66FF")], startPoint: .top, endPoint: .bottom)
         default:
             return LinearGradient(colors: [Color(hex: "#AFFFA1"), Color(hex: "#85FF91")], startPoint: .top, endPoint: .bottom)
         }
